070ed0445fad0571f697273e27071966c5a06ce0
jxmai <jxmai@users.noreply.github.com>
2018-10-23 07:49:50 -0500

    more tests for transpose

--- Transpose.py
+++ Transpose.py
@@ -9,8 +9,17 @@ def accessTranspose(sequence = [], inputList = []):
 
 
 class TestTranspose(unittest.TestCase):
-    def test_transpose(self):
+    def test_transpose_1(self):
         self.assertEqual(accessTranspose([5], [1,2,3,4,5]), [1,2,3,5,4])
+
+    def test_transpose_2(self):
+        self.assertEqual(accessTranspose([5,5], [1,2,3,4,5]), [1,2,5,3,4])
+
+    def test_transpose_3(self):
+        self.assertEqual(accessTranspose([5,5,2], [1,2,3,4,5]), [2,1,5,3,4])
+
+    def test_transpose_4(self):
+        self.assertEqual(accessTranspose([5,5,2,2], [1,2,3,4,5]), [2,1,5,3,4])
 
 if __name__== "__main__":
     unittest.main()
