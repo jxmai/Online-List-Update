d5983f4c0bc12c8a4db15917d17b180a9b2ecd2e
jxmai <jxmai@users.noreply.github.com>
2018-10-22 19:02:47 -0500

    main.py

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import random
 import unittest
 import copy
 import MTF
-
+import decreasingFreqList
+from fractions import Fraction
 
 
 def main():
-    theList = random.sample(xrange(10000),10)
+    theList = random.sample(xrange(10000),500)
     # print(theList)
     sequence = []
 
@@ -18,9 +19,10 @@ def main():
     # for i in range(100):
     #     sequence.append(theList[random.randint(0,len(theList)-1)])
     # print sequence
-    cost = (MTF.moveToFront(sequence, theList))[1]
-    print(cost)
+    mtf_cost = (MTF.moveToFront(sequence, theList))[1]
+    fc_cost = (decreasingFreqList.accessDecreasingFreqList(sequence, theList))[1]
 
+    print('competitive ratio between MTF and FC is {0}'.format(float(mtf_cost)/ float(fc_cost)))
 
 if __name__== "__main__":
     main()
