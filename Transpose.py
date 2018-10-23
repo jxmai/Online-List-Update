127c70959bbc09bd55e0aac4dbe51227dfb49d00
jxmai <jxmai@users.noreply.github.com>
2018-10-23 08:42:10 -0500

    test

--- Transpose.py
+++ Transpose.py
@@ -11,16 +11,18 @@ def accessTranspose(sequence = [], inputList = []):
                 totalCost += i + 1
                 if i != 0:
                     outputList.insert(i-1, outputList.pop(i))
-                    totalCost += i + 1
+                    totalCost += 1
     
     return outputList, totalCost
 
 class TestTranspose(unittest.TestCase):
     def test_transpose_1(self):
         self.assertEqual(accessTranspose([5], [1,2,3,4,5])[0], [1,2,3,5,4])
+        self.assertEqual(accessTranspose([5], [1,2,3,4,5])[1], 6)
 
     def test_transpose_2(self):
         self.assertEqual(accessTranspose([5,5], [1,2,3,4,5])[0], [1,2,5,3,4])
+        self.assertEqual(accessTranspose([5,5], [1,2,3,4,5])[1], 6 + 5)
 
     def test_transpose_3(self):
         self.assertEqual(accessTranspose([5,5,2], [1,2,3,4,5])[0], [2,1,5,3,4])
