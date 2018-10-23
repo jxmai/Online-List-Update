0edcb93a13c9edd15317dc0a33e802d7265dcd24
jxmai <jxmai@users.noreply.github.com>
2018-10-23 08:40:37 -0500

    cost impl

--- Transpose.py
+++ Transpose.py
@@ -4,26 +4,29 @@ import copy
 def accessTranspose(sequence = [], inputList = []):
     outputList = copy.deepcopy(inputList)
     
+    totalCost = 0
     for s in sequence:
         for i in range(len(outputList)):
             if outputList[i] == s:
+                totalCost += i + 1
                 if i != 0:
                     outputList.insert(i-1, outputList.pop(i))
+                    totalCost += i + 1
     
-    return outputList
+    return outputList, totalCost
 
 class TestTranspose(unittest.TestCase):
     def test_transpose_1(self):
-        self.assertEqual(accessTranspose([5], [1,2,3,4,5]), [1,2,3,5,4])
+        self.assertEqual(accessTranspose([5], [1,2,3,4,5])[0], [1,2,3,5,4])
 
     def test_transpose_2(self):
-        self.assertEqual(accessTranspose([5,5], [1,2,3,4,5]), [1,2,5,3,4])
+        self.assertEqual(accessTranspose([5,5], [1,2,3,4,5])[0], [1,2,5,3,4])
 
     def test_transpose_3(self):
-        self.assertEqual(accessTranspose([5,5,2], [1,2,3,4,5]), [2,1,5,3,4])
+        self.assertEqual(accessTranspose([5,5,2], [1,2,3,4,5])[0], [2,1,5,3,4])
 
     def test_transpose_4(self):
-        self.assertEqual(accessTranspose([5,5,2,2], [1,2,3,4,5]), [2,1,5,3,4])
+        self.assertEqual(accessTranspose([5,5,2,2], [1,2,3,4,5])[0], [2,1,5,3,4])
 
 if __name__== "__main__":
     unittest.main()
